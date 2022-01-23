ecbd872bd056779d7409c3dcfbbc350a882a3b7e
eminvergil <emintrhack@gmail.com>
2022-01-23 23:43:10 +0300

    change description on docusaurus.config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,10 +6,10 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
+  title: "Emin Vergil's blog",
+  tagline: "",
+  url: "https://eminvergil.github.io",
+  baseUrl: "/blog/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
