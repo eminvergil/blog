c7e7ce09e4fcff860483f86e4c437db2421c4046
eminvergil <emintrhack@gmail.com>
2022-01-29 18:43:40 +0300

    Change baseUrl to blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,15 +1,12 @@
 // @ts-nocheck
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
-
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Emin Vergil's blog",
   tagline: "",
   url: "https://eminvergil.github.io",
-  baseUrl: "/etc/home/",
+  baseUrl: "/blog/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
